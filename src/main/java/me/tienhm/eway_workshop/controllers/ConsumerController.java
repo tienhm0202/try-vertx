2d483ec5200afa08c3e4308c2f684079486d59e7
Hoang Manh Tien <tienhm.0202@gmail.com>
2020-02-28 01:16:38 +0700

    fixup! add: consumer controller handling Message from EventBus

--- src/main/java/me/tienhm/eway_workshop/controllers/ConsumerController.java
+++ src/main/java/me/tienhm/eway_workshop/controllers/ConsumerController.java
@@ -6,10 +6,14 @@ import me.tienhm.eway_workshop.DB;
 public class ConsumerController {
   public static void addCandidate(Message<Object> message) {
     try {
-      Thread.sleep(60000);
+      Thread.sleep(5000);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    DB.addCandidate(message.body().toString());
+    String candidateName = message.body().toString();
+    System.out.println("Message: " + candidateName);
+    if (DB.getCandidateByName(candidateName).equals("candidate not found")) {
+      DB.addCandidate(candidateName);
+    }
   }
 }
